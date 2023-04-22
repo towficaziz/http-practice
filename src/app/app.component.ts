d5af15b2b5dbf55ed6be58440a0424a81d690f29
towficaziz <tapolly01@gmail.com>
2023-04-23 00:28:37 +0200

    Sending a POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,12 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      'https://http-request-fe9c9-default-rtdb.firebaseio.com/posts.json',
+    postData
+    ).subscribe(responseData =>{
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
