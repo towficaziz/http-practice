d1d83cae4582e3d9329f591fe4c3497a7061ca51
towficaziz <tapolly01@gmail.com>
2023-04-23 00:50:55 +0200

    Using RxJS Operators to transform Response Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -36,7 +37,18 @@ export class AppComponent implements OnInit {
 
   private fetchPosts(){
     this.http.get('https://http-request-fe9c9-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for (const key in responseData) {
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key], id: key});
+        }
+      }
+      return postsArray;
+    })
+    )
     .subscribe(posts =>{
+
       console.log(posts);
     });
   }
